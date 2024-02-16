74b875e57f8aa4f94347094273fcf082a38ae528
mayur11 <mayurchauhan1107@gmail.com>
2024-02-16 12:06:39 +0530

    added language feature

--- app.js
+++ app.js
@@ -8,8 +8,21 @@ const searchResult = document.querySelector(".search-result");
 const example = document.querySelector(".example");
 const warning = document.querySelector(".warning");
 
+function langSelect () {
+  var buttons = document.getElementsByClassName('tab-button');
+  
+  for (var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('change', function() {
+          var checkedButton = document.querySelector('.tab-button:checked + label');
+          console.log('Selected button:', checkedButton.textContent);
+      });
+  }
+};
+
+
 // API URL for fetching dictionary data
-const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const SEARCH_API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+// const TRANSLATION_API = "https://api.mymemory.translated.net/get?q=Hello%20World!&langpair=en|hi";
 
 // Event listener for search button
 searchBtn.addEventListener("click", async (e) => {
@@ -19,7 +32,7 @@ searchBtn.addEventListener("click", async (e) => {
 
   try {
     // Fetch data from the API based on the search text
-    const res = await fetch(API_URL + searchText);
+    const res = await fetch(SEARCH_API_URL + searchText);
     const data = await res.json();
 
     // Extract relevant information from the API response
@@ -32,7 +45,7 @@ searchBtn.addEventListener("click", async (e) => {
     outputWord.textContent =
       resultWord.charAt(0).toUpperCase() + resultWord.slice(1);
     partOfSpeech.textContent = noun;
-    searchResult.textContent = meaning;
+    // searchResult.textContent = meaning;
     example.textContent = exampleLine;
 
     // Find and set audio data if available
@@ -55,9 +68,59 @@ searchBtn.addEventListener("click", async (e) => {
       // console.error("Audio data is missing in the API response.");
       // Handle the missing audio data, e.g., by displaying an error message or fallback content.
     }
+
+    let checkedButton = document.querySelector('.tab-button:checked + label');
+
+    if(checkedButton.textContent == "Hindi") {
+      const transRes = await fetch(`https://api.mymemory.translated.net/get?q=${meaning}!&langpair=en|hi`);
+      const transData = await transRes.json();
+      console.log(transData.responseData.translatedText);
+      
+      searchResult.textContent = transData.responseData.translatedText;
+
+      if(exampleLine !== undefined) {
+        const transRes = await fetch(`https://api.mymemory.translated.net/get?q=${exampleLine}!&langpair=en|hi`);
+        const transData = await transRes.json();
+        console.log(transData.responseData.
+        translatedText
+        );
+        example.textContent = transData.responseData.translatedText;
+    }
+    } else if (checkedButton.textContent == "Gujarati") {
+      const transRes = await fetch(`https://api.mymemory.translated.net/get?q=${meaning}!&langpair=en|gu`);
+      const transData = await transRes.json();
+      console.log(transData.responseData.
+        translatedText
+        );
+      searchResult.textContent = transData.responseData.
+      translatedText;
+
+      if(exampleLine !== undefined) {
+        const transRes = await fetch(`https://api.mymemory.translated.net/get?q=${exampleLine}!&langpair=en|gu`);
+        const transData = await transRes.json();
+        console.log(transData.responseData.
+        translatedText
+        );
+        example.textContent = transData.responseData.translatedText;
+      }
+    
+    } else {
+      searchResult.textContent = meaning;
+    }
+    console.log(checkedButton);
+    // const transRes = await fetch(`https://api.mymemory.translated.net/get?q=${meaning}!&langpair=en|hi`);
+    // const transData = await transRes.json();
+    // console.log(transData.responseData.
+    //     translatedText
+    //     );
+    
+      
+    //   searchResult.textContent = transData.responseData.
+    //   translatedText;
+    
   } catch (e) {
     // Handle errors in fetching data from the API
-    const res = await fetch(API_URL + searchText);
+    const res = await fetch(SEARCH_API_URL + searchText);
     const data = await res.json();
 
     outputWord.textContent = data.title;
